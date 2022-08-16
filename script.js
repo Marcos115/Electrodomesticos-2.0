0782945b96f1d62c9c022229e19b2d53545ad9ab
Marcos Amador <marcosamador8@hotmail.com>
2022-08-16 12:56:35 -0300

    fetch y hoover

--- script.js
+++ script.js
@@ -161,6 +161,42 @@ carritoLogo.addEventListener("click", () => {
     })
 })
 
+const tableId = document.getElementById("tableId")
+const boton1 = document.getElementById("boton1")
+
+async function mostrarProximamente() {
+    const productos = await fetch('./json/productos.json')
+    const productosParseados = await productos.json()
+    tableId.innerHTML = `
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Modelo</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Imagen</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+            <tbody id="tBody">
+            </tbody>
+        </table>
+    
+    `
+    productosParseados.forEach((producto, indice) => {
+        tBody.innerHTML += `
+            <tr id="producto${indice + 1}">
+                <th scope="row">${indice + 1}</th>
+                <td>${producto.nombre}</td>
+                <td>${producto.modelo}</td>
+                <td>$${producto.precio}</td>
+                <td><img src="./img/${producto.img}" class = "imagenes"></td>
+            </tr>
+        `
+    })
+}
+
 //Tema
 
 const botonDarkMode = document.getElementById("botonDarkMode")
@@ -185,3 +221,4 @@ botonLightMode.addEventListener("click", () => {
     document.body.classList.remove('darkMode')
     localStorage.setItem('theme', "light")
 })
+
